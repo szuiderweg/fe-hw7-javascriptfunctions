fc1f66b29890c9da6776dd6f7f69cfc538a8a254
szuiderweg <svmzuiderweg@hotmail.com>
2024-01-17 21:59:40 +0100

    opdracht 2 en 3 van deel 2 af

--- functions-methods.js
+++ functions-methods.js
@@ -15,16 +15,12 @@ function getEmailDomainName(email){
     const fragments = email.split("@");//gebruik de method String.split om het email adress op te knippen in stukjes met "@" als seperator
     return fragments[1];//de fragments array bestaat altijd uit 2 entries omdat een emailadress altijd precies 1 "@" heeft. De domeinnaam is dus altijd de String op indexwaarde 1.
 }
-
+console.log("output voor opdracht 1");
 console.log(getEmailDomainName("n.eeken@novi-education.nl"));
 console.log(getEmailDomainName("t.mellink@novi.nl"));
 console.log(getEmailDomainName("a.wiersma@outlook.com"));
 
-
-
-
-
-//todo opdracht 2.2
+console.log("");
 
 /* Opdracht  2 */
 // Schrijf een functie genaamd typeOfEmail, die een emailadres verwacht. De functie checkt of het emailadres een novi domein heeft (medewerker), een novi-education domein (student), of qextern domein (zoals gmail of outlook)
@@ -34,17 +30,31 @@ console.log(getEmailDomainName("a.wiersma@outlook.com"));
 // typeOfEmail("novi.nlaapjesk@outlook.com") geeft geeft "Extern" <-- deze moet het ook doen!
 // typeOfEmail("a.wiersma@outlook.com") geeft "Extern"
 
-//in: een email adres
-//uit: string die soort domein omschrijft
-//stapjes
-//haal de domein naam uit het e-mail adres mbv de functie uit de vorige opdracht
-//maak een variabele aan voor het resultaat
-//check met if, elde if en else wat voor domein het is en stop het antwoord in het resultaat
-//print resultaat
-//stop dit alles in een console.log
+function typeOfEmail(email)
+{
+    const domain = getEmailDomainName(email);//haal de domeinnaam uit het e-mailadres mbv de functie uit de vorige opdracht
+    if(domain === "novi-education.nl")//als de domeinnaam precies gelijk is aan novi-education.nl
+    {
+        return  "Student";//geef "Student" terug uit de functie
+    }
+    else if(domain ==="novi.nl")//als de domeinnaam precies gelijk is aan novi.nl
+    {
+        return "Medewerker";//geef "Medewerker" terug uit de functie
+    }
+    else
+    {
+        return "Extern"; //in andere gevallen is het domein extern
+    }
+}
+
+console.log("output voor opdracht 2");
+console.log(typeOfEmail("n.eeken@novi-education.nl"));
+console.log(typeOfEmail("t.mellink@novi.nl"));
+console.log(typeOfEmail("novi.nlaapjesk@outlook.com"));
+console.log(typeOfEmail("a.wiersma@outlook.com"));
+console.log("");
 
 
-//todo opdracht 2.3
 /* Opdracht  3 */
 // Schrijf een functie genaamd checkEmailValidity, die een emailadres verwacht en checkt of het emailadres valide is. De functie returned true of false, afhankelijk van de uitkomst.
 // Een emailadres is valide wanneer:
@@ -58,20 +68,62 @@ console.log(getEmailDomainName("a.wiersma@outlook.com"));
 // checkEmailValidity("n.eeken@novinl.") geeft false - want de punt mag niet als laatst
 // checkEmailValidity("tessmellink@novi,nl") geeft false - want er staat een komma in
 
+//in: een string die mogelijk een emailadress is
+//out: true of false
+//stapjes:
+//stel een boolean valid = false;
+//1. neem een string possibleEmail
+
+function checkEmailValidity(possibleEmail) {
+    let valid; //dit wordt de return value (boolean) van de functie
+    //2. check of er precies 1 "@" in voorkomt
+    const fragments = possibleEmail.split("@");
+    if (fragments.length !== 2) {
+        return valid = false;//als er precies 1 keer "@" voorkomt is de lengte van het resulterende array altijd 2. bij een andere waarde bevat het adres geen of te vaak "@". Dan hoeven we niet ook verder te kijken naar andere criteria.
+    }
+
+    //3. als de opgegeven string een "," bevat, is het geen geldig e-mail adres en returnt de functie False
+    if(possibleEmail.includes(","))
+    {
+        return valid = false;//
+    }
+
+    //4. check of in de domeinnaam een"." bevat en dit niet het laatste teken is.
+    const domainName = getEmailDomainName(possibleEmail);//pak de domeinnaam uit het e-mail adres mbv functie uit opdracht 1
+
+    if(domainName.includes(".")===false)//check de domeinnaam een "." bevat. Zonder "." is het geen geldig adres
+    {
+        return valid = false;
+    }
+
+    if(domainName.indexOf(".") === (domainName.length-1))//check of de "." op de laatste plaats staat. Zo ja is het adres ongeldig
+    {
+        return valid = false;
+    }
+
+    return valid = true;//als deze regel wordt bereikt, zijn alle checks geslaagd en is de String in de functieparameter dus een geldig emailadres
+}
+
+console.log("output van opdracht 3")
+// checkEmailValidity("n.eeken@novi.nl") geeft true - want @ en punt op de juiste plek
+console.log(checkEmailValidity("n.eeken@novi.nl"));
+// checkEmailValidity("tessmellink@novi.nl") geeft true - want @ en punt op de juiste plek
+console.log(checkEmailValidity("tessmellink@novi.nl"));
+// checkEmailValidity("n.eekenanovi.nl") geeft false - want geen @
+console.log(checkEmailValidity("n.eekenanovi.nl"));
+// checkEmailValidity("n.eeken@novinl.") geeft false - want de punt mag niet als laatst
+console.log(checkEmailValidity("n.eeken@novinl."));
+// checkEmailValidity("tessmellink@novi,nl") geeft false - want er staat een komma in
+console.log(checkEmailValidity("tessmellink@novi,nl"));
+
+
+
+
+
+
+
+
+
+
 //todo beide scripts koppelen aan een index.html en testen of ze in de console draaien
 
-//todo gevraagd in huiswerkklas geldigheid email checken komt in de volgende opdrachten! hier hoef je alleen aaaa.nl uit de string te vissen
-//controleer of het format klopt:
-//hak het op in stukjes
-//een e-mail adres bevat
-//recipient name (stuk voor @)
-//domeinnaam
-//toplevel domain.
-//regels: het bevat een @
-//@ is niet het eerste teken
-//@ is hooguit het op 4 na laatste teken (kortst mogelijke domeinnaam is a.aa )
-//het bevat een '.'
-// '.' is niet het eerste teken
-// '.' is hooguit het twee na laatste teken
-//als het adres geldig is , return het stuk na de @
-//als het adres niet-gelidg is , print een melding " ongeldig e-mail adres"
